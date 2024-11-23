8aa6d3cdd6a5591620a715eaa230a9ac835860e1
bjarkiella <bjarkiella@gmail.com>
2024-11-23 21:46:33 +0100

    Just missing the logistic one

--- src/main/java/com/satisfactorycalculator/models/Logistics.java
+++ src/main/java/com/satisfactorycalculator/models/Logistics.java
@@ -1,5 +1,12 @@
+// This class handles the logistic belts and returns the most suitible given the requested capacity
+
 package com.satisfactorycalculator.models;
 
+
+
+
 public class Logistics {
-    
+// Instead of filtering the item name, the capacity should be used as a filter    
 }
+
+
